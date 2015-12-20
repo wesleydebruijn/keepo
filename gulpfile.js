5fbdb2718ddc0cce367ba6d5c60c8741476e18e6
Wesley de Bruijn <wesley.dbr@live.nl>
2015-12-20 18:55:21 +0100

    Watch CSS/JS gulp file + server start gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var minify = require('gulp-minify-css');
+var express = require('express');
 
 var libs = [
     'node_modules/angular2/bundles/angular2-polyfills.js',
@@ -15,7 +16,7 @@ var libs = [
 
 var styling = [
     'node_modules/materialize-css/dist/css/materialize.min.css',
-    'public/assets/css/**/*'
+    'public/assets/css/*'
 ];
 
 gulp.task('build-lib', function() {
@@ -35,14 +36,38 @@ gulp.task('build-core', function() {
 
 gulp.task('css', function() {
     gulp.src(styling)
-        .pipe(concat())
-        .pipe(minify('app.min.css'))
-        .pipe(gulp.dest('build'))
-})
+        .pipe(concat('app.min.css'))
+        .pipe(minify())
+        .pipe(gulp.dest('./build'))
+});
 
 gulp.task('watch', function() {
-    gulp.watch('public/css/**/*', ['style']);
+    gulp.watch('public/assets/css/*', ['css']);
     gulp.watch('app/**/*.ts', ['build-core']);
-})
+});
 
-gulp.task('default', ['build-lib', 'build-core']);
+// Start Server
+gulp.task('server', function() {
+  var app = express();
+
+  // Server static files
+  app.use(express.static(__dirname + '/public'));
+  app.use('/build', express.static(__dirname + '/build'));
+
+  // Set jade as our renderer
+  app.set('view engine', 'jade');
+  app.set('views', __dirname);
+
+  // Router
+  app.get('/', function (req, res) {
+    res.render('./app/index', { title: 'Hey', message: 'Hello world!'});
+  });
+
+  // Register server
+  var server = app.listen(3000, function() {
+  	var host = server.address().address;
+  	var port = server.address().port;
+  });
+});
+
+gulp.task('default', ['server', 'watch']);
