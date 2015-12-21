563db57a6122aece3796edeb8d863271ff4b6712
jvhoven <wolferine@live.se>
2015-12-21 22:10:56 +0100

    Added router

--- app/boot.ts
+++ app/boot.ts
@@ -1,4 +1,19 @@
 import {bootstrap} from 'angular2/platform/browser'
-import {SessionComponent} from './components/session.component'
+import {SessionComponent} from './components/session.component';
+import {AppComponent} from './components/app.component';
+import {Component, provide} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 
-bootstrap(SessionComponent)
+@Component({
+    selector: 'basic-routing',
+    template: `<a [routerLink]="['/Home']">Home</a>
+               <a [routerLink]="['/Session']">Session</a>
+               <router-outlet></router-outlet>`,
+    directives: [ROUTER_DIRECTIVES]})
+@RouteConfig([
+    {path: '/', component: AppComponent, as: 'Home'},
+    {path: '/session', component: SessionComponent, as: 'Session' }])
+class RootComponent {}
+
+bootstrap(RootComponent, [ROUTER_PROVIDERS,
+  provide(LocationStrategy, {useClass: HashLocationStrategy})]);
