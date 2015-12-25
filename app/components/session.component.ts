d36611bf7367abf681b1fee7b2d1e7cb5b991967
jvhoven <wolferine@live.se>
2015-12-25 17:11:07 +0100

    Better error messages declaration

--- app/components/session.component.ts
+++ app/components/session.component.ts
@@ -1,6 +1,11 @@
 import { Component, View } from 'angular2/core';
 import { SessionService } from '../services/session.service';
 
+const ERROR_TOO_SHORT = "Session name should be longer than 6 characters";
+const ALREADY_IN_SESSION = "Already in session";
+const NO_SESSION = "No session";
+const EMPTY_SESSION = "";
+
 interface Session {
   name: string,
   error: string
@@ -15,34 +20,52 @@ interface Session {
 })
 export class SessionComponent {
   public session : Session = {
-    name: "No session",
+    name: NO_SESSION,
     error: ""
   };
   public sessionName: string;
   constructor(private sessionService: SessionService) {
   }
 
+  isCorrectSession() : boolean {
+    this.session.error = EMPTY_SESSION;
+    if(this.sessionName == this.session.name) {
+      this.session.error = ALREADY_IN_SESSION;
+      return false;
+    } else if(this.sessionName.length < 6) {
+      this.session.error = ERROR_TOO_SHORT;
+      return false;
+    }
+    return true;
+  }
+
   createSession() {
-    var name = this.sessionName;
-    this.sessionService.createSession(name).subscribe(
-      (data) => {
-        this.session.name = data.name;
-        this.session.error = "";
-      }
-    );
+    if(this.isCorrectSession()) {
+      var sessionName = this.sessionName;
+      this.sessionService.createSession(sessionName).subscribe(
+        (data) => {
+          if(data.error) {
+            this.session.error = data.error;
+          } else {
+            this.session.name = data.name;
+          }
+        }
+      );
+    }
   }
 
   joinSession() {
-    var name = this.sessionName;
-    this.sessionService.joinSession(name).subscribe(
-      (data) => {
-        if(data.error) {
-          this.session.error = data.error;
-        } else {
-          this.session.error = "";
-          this.session.name = data.name;
+    if(this.isCorrectSession()) {
+      var sessionName = this.sessionName;
+      this.sessionService.joinSession(sessionName).subscribe(
+        (data) => {
+          if(data.error) {
+            this.session.error = data.error;
+          } else {
+            this.session.name = data.name;
+          }
         }
-      }
-    );
+      );
+    }
   }
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,9 @@ var models  = require('../sequelize/models');
 var mongoose = require('../mongoose/database');
 var router = express.Router();
 
+const SESSION_NOT_FOUND = "Session is not found";
+const SESSION_ALREADY_EXISTS = "Session already exists";
+
 // Sequelize test
 router.get('/sequelize', function(req, res, next) {
   models.User.findAll().then(function(users) {
@@ -13,9 +16,10 @@ router.get('/sequelize', function(req, res, next) {
 // Mongoose test
 router.post('/session/join', function(req, res, next) {
   var Session = mongoose.model('Session', mongoose.Session);
+
   Session.findOne({ 'name': req.body.name }, function(err, data) {
     if(data == null) {
-      res.send(JSON.stringify({ error: 'Not found' }));
+      res.send(JSON.stringify({ error: SESSION_NOT_FOUND }));
     } else {
       res.send(data);
     }
@@ -28,12 +32,19 @@ router.post('/session/create', function(req, res, next) {
     name: req.body.name
   });
 
-  session.save(function(err, session) {
-    if(err) console.log(err);
-    Session.findById(session._id, function(err, session) {
-      res.send(session);
-    });
-  })
+  Session.findOne({ 'name': req.body.name }, function(err, data) {
+    if(data != null) {
+      res.send(JSON.stringify({ error: SESSION_ALREADY_EXISTS }));
+      return;
+    } else {
+      session.save(function(err, session) {
+        if(err) console.log(err);
+        Session.findById(session._id, function(err, session) {
+          res.send(session);
+        })
+      });
+    }
+  });
 })
 
 module.exports = router;
